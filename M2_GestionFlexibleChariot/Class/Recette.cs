fe16eca9aeeb0333feb08880d33ee01d74b44e2d
sagarion <thibault.daucourt@gmail.com>
2019-09-09 11:28:56 +0200

    implémentation Recette

--- M2_GestionFlexibleChariot/Class/Recette.cs
+++ M2_GestionFlexibleChariot/Class/Recette.cs
@@ -12,5 +12,74 @@ namespace M2_GestionFlexibleChariot.Class
 {
     class Recette
     {
+        private int identifiant;
+        private string libellé;
+        private Pas[] Pas;
+        private DateTime dateCréation;
+
+        // Constructeur
+
+        /// <summary>
+        /// contructeur pour un recette importé depuis la base de données
+        /// </summary>
+        /// <param name="identifiant"> identifiant de gestion de la base de données</param>
+        /// <param name="libellé"> libellé décrivant la recette </param>
+        /// <param name="pas"> tableau stockant les pas(opération) de la recette </param>
+        /// <param name="dateCréation"> date de la création de la recette </param>
+        public Recette(int identifiant, string libellé, Pas[] pas, DateTime dateCréation)
+        {
+            this.identifiant = identifiant;
+            this.libellé = libellé;
+            Pas = pas;
+            this.dateCréation = dateCréation;
+        }
+
+        /// <summary>
+        /// constructeur pour une nouvelle recette
+        /// </summary>
+        /// <param name="libellé"> libellé décrivant la recette </param>
+        public Recette(string libellé)
+        {
+            this.libellé = libellé;
+            Pas = new Pas[10];
+            this.dateCréation = DateTime.Now;
+        }
+
+        // Getter / Setter
+        public int GetIdentifiant()
+        {
+            return identifiant;
+        }
+
+        public string GetLibellé()
+        {
+            return libellé;
+        }
+
+        public void SetLibellé(string libellé)
+        {
+            this.libellé = libellé;
+        }
+
+        public DateTime GetDateCréation()
+        {
+            return dateCréation;
+        }
+
+        public Pas[] getPas()
+        {
+            return Pas;
+        }
+
+        /// <summary>
+        /// modifie le pas à l'index donnée avec les information fournis
+        /// </summary>
+        /// <param name="pas"></param>
+        /// <param name="index"></param>
+        public void SetPas(Pas pas, int index)
+        {
+            this.Pas[index] = pas;
+        }
+
     }
 }
