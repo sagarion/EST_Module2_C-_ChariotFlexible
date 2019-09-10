52fdbae47fe7d3ec3789f3034f41b8523594de1b
sagarion <thibault.daucourt@gmail.com>
2019-09-10 14:56:48 +0200

    BDD v1 -implémentation mécanisme de connection -implémentation de l'import d'une recette
    les deux fonctions sont non-testées

---
+++ M2_GestionFlexibleChariot/BDD/BDDConnexion.cs
@@ -0,0 +1,63 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace M2_GestionFlexibleChariot.BDD
+{
+    class BDDConnexion
+    {
+        private const string server = "localhost";
+        private const string database = "testDB" ;
+        private const string uid = "root" ;
+        private const string pwd = "abc123" ;
+
+        private static MySqlConnection connexion = null;
+
+        /// <summary>
+        /// Donne accès à une connection et la crée s'il n'existe pas encore
+        /// </summary>
+        /// <returns></returns>
+        public static MySqlConnection GetConnection()
+        {
+            if (connexion == null)
+            {
+                Connecter();
+            }
+
+            return connexion;
+        }
+
+
+        /// <summary>
+        /// créer la connexion à la base de donnée selon les constante de cette classe
+        /// </summary>
+        public static void Connecter()
+        {
+            try
+            {
+                string connexionString = $"server={server};database={database};uid={uid};pwd={pwd};";
+                connexion = new MySqlConnection(connexionString);
+                connexion.Open();
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Déconnecte la connexion à la base de donnée
+        /// </summary>
+        public static void Deconnecter()
+        {
+            try
+            {
+                connexion.Close();
+                connexion = null;
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ M2_GestionFlexibleChariot/BDD/BDDRecette.cs
@@ -0,0 +1,69 @@
+﻿using M2_GestionFlexibleChariot.Class;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M2_GestionFlexibleChariot.BDD
+{
+    class BDDRecette
+    {
+        /// <summary>
+        /// Importe les données d'une recette depuis la base de donnée et créer un objet correspondant
+        /// </summary>
+        /// <param name="identifiant"> identifiant de la recette à importer et créer </param>
+        /// <returns> un objet Recette équivalent à la recette en base de données correspondant à l'identifant fourni</returns>
+        public static Recette GetRecette(int identifiant)
+        {
+            Recette recette = null;
+
+            try
+            {
+                // création des requêtes, execution de celles-ci et import des résultat dans les curseurs
+                string sqlRecette = $"SELECT identifiant,libelle, date création FROM Recette WHERE index='{identifiant}'";
+                MySqlCommand command = new MySqlCommand(sqlRecette, BDDConnexion.GetConnection());
+                MySqlDataReader readerRecette = command.ExecuteReader();
+
+                string sqlPas = $"SELECT identifiant,index,temps, position, libellé, quittance FROM Pas WHERE Recette_identifiant='{identifiant}'";
+                command = new MySqlCommand(sqlPas, BDDConnexion.GetConnection());
+                MySqlDataReader readerPas = command.ExecuteReader();
+
+
+                Pas[] pas = new Pas[10];
+                int index = 0;
+
+                // parcours le curseur jusqu'à créer le tableau complet de Pas pour la recette
+                while (readerPas.Read())
+                {
+                    pas[index] = new Pas(int.Parse(readerPas[0].ToString()),
+                        int.Parse(readerPas[1].ToString()),
+                        int.Parse(readerPas[2].ToString()),
+                        int.Parse(readerPas[3].ToString()),
+                        readerPas[4].ToString(),
+                        bool.Parse(readerPas[5].ToString()));
+
+                    index++;
+                }
+                // ferme le curseur de pas
+                readerPas.Close();
+
+                // une fois le tableau de pas créé, créer l'objet recette
+                recette = new Recette(int.Parse(readerRecette[0].ToString()),
+                                                 readerRecette[1].ToString(),
+                                                pas,
+                                                DateTime.Parse(readerRecette[2].ToString())
+                                                );
+                // ferme le curseur de recette
+                readerRecette.Close();
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.Message);
+            }
+
+            return recette;
+        }
+    }
+}
